2d6674edaeeba7a6dcaa07e0156c4b2ef40146c9
TomasAMolinari <tomasmolinari_2008@hotmail.com>
2024-08-12 01:55:09 -0300

    debug __file__ path

--- api/config/config.py
+++ api/config/config.py
@@ -2,6 +2,8 @@ import json
 import os
 from google.oauth2 import service_account
 
+print("---------------------------Ruta de ejecución:---------------------------", os.path.dirname(__file__))  # Verifica la ruta desde donde se está intentando abrir el archivo
+
 # ruta al archivo de credenciales
 CREDENTIALS_FILE = os.path.join(os.path.dirname(__file__), 'gcp-terraform-creds.json')
 
@@ -9,7 +11,7 @@ CREDENTIALS_FILE = os.path.join(os.path.dirname(__file__), 'gcp-terraform-creds.
 credentials = service_account.Credentials.from_service_account_file(CREDENTIALS_FILE)
 
 # cargar configuración de variables de GCP
-with open(os.path.join(os.path.dirname(__file__), '../gcp_vars.json'), 'r') as config_file:
+with open(os.path.join(os.path.dirname(__file__), 'gcp_vars.json'), 'r') as config_file:
     gcp_vars = json.load(config_file)
 
 # configuración para Flask
